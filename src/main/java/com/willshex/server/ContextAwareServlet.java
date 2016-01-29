c2654c8e9529fe1e946ba15eaff4d40334d2f156
billy1380 <william.shakour@gmail.com>
2016-01-29 22:08:09 +0000

    added pom file and git ignore

---
+++ src/main/java/com/willshex/server/ContextAwareServlet.java
@@ -0,0 +1,140 @@
+//
+//  ContextAwareServlet.java
+//  willshex-server
+//
+//  Created by William Shakour on 19 Jul 2013.
+//  Copyright © 2013 WillShex Limited. All rights reserved.
+//
+package com.willshex.server;
+
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author billy1380
+ * 
+ */
+@SuppressWarnings("serial")
+public class ContextAwareServlet extends HttpServlet {
+
+	public static final ThreadLocal<HttpServletRequest> REQUEST = new ThreadLocal<HttpServletRequest>();
+	public static final ThreadLocal<HttpServletResponse> RESPONSE = new ThreadLocal<HttpServletResponse>();
+	public static final ThreadLocal<ServletContext> CONTEXT = new ThreadLocal<ServletContext>();
+
+	/* (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doDelete(javax.servlet.http.
+	 * HttpServletRequest, javax.servlet.http.HttpServletResponse) */
+	@Override
+	protected void doDelete (HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		REQUEST.set(req);
+		RESPONSE.set(resp);
+		CONTEXT.set(this.getServletContext());
+
+		doDelete();
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.
+	 * HttpServletRequest, javax.servlet.http.HttpServletResponse) */
+	@Override
+	protected void doGet (HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		REQUEST.set(req);
+		RESPONSE.set(resp);
+		CONTEXT.set(this.getServletContext());
+
+		doGet();
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doHead(javax.servlet.http.
+	 * HttpServletRequest, javax.servlet.http.HttpServletResponse) */
+	@Override
+	protected void doHead (HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		REQUEST.set(req);
+		RESPONSE.set(resp);
+		CONTEXT.set(this.getServletContext());
+
+		doHead();
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doOptions(javax.servlet.http.
+	 * HttpServletRequest, javax.servlet.http.HttpServletResponse) */
+	@Override
+	protected void doOptions (HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		REQUEST.set(req);
+		RESPONSE.set(resp);
+		CONTEXT.set(this.getServletContext());
+
+		doOptions();
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.
+	 * HttpServletRequest, javax.servlet.http.HttpServletResponse) */
+	@Override
+	protected void doPost (HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		REQUEST.set(req);
+		RESPONSE.set(resp);
+		CONTEXT.set(this.getServletContext());
+
+		doPost();
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPut(javax.servlet.http.
+	 * HttpServletRequest, javax.servlet.http.HttpServletResponse) */
+	@Override
+	protected void doPut (HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		REQUEST.set(req);
+		RESPONSE.set(resp);
+		CONTEXT.set(this.getServletContext());
+
+		doPut();
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doTrace(javax.servlet.http.
+	 * HttpServletRequest, javax.servlet.http.HttpServletResponse) */
+	@Override
+	protected void doTrace (HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		REQUEST.set(req);
+		RESPONSE.set(resp);
+		CONTEXT.set(this.getServletContext());
+
+		doTrace();
+	}
+
+	protected void doDelete () throws ServletException, IOException {}
+
+	protected void doGet () throws ServletException, IOException {}
+
+	protected void doHead () throws ServletException, IOException {}
+
+	protected void doOptions () throws ServletException, IOException {}
+
+	protected void doPost () throws ServletException, IOException {}
+
+	protected void doPut () throws ServletException, IOException {}
+
+	protected void doTrace () throws ServletException, IOException {}
+
+}
